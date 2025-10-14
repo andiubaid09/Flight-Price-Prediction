e7bc965eaf6ebd60c15357f8bd38253489f53b31
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-14 20:08:55 +0800

    Update src Training

--- Linear Regression/src/Training.py
+++ Linear Regression/src/Training.py
@@ -1,2 +1,15 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.compose import TransformedTargetRegressor
+
+regressor = LinearRegression()
+
+model_linear = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("regressor", TransformedTargetRegressor(
+        regressor = regressor,
+        func = np.log1p,
+        inverse_func = np.expm1
+    ))
+])
 model_linear.fit(X_train, y_train)
 print(model_linear)

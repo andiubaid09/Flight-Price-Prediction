960931507aa754c8a353386baf0114438c9c39a6
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-15 17:21:48 +0800

    Add src Evaluate.py

---
+++ Lasso Regressor/src/Evaluate.py
@@ -0,0 +1,37 @@
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import matplotlib as plt
+import seaborn as plt
+
+print('Best params lasso :', grid_lasso.best_params_)
+print('Best CV Score :', grid_lasso.best_score_)
+print(best_model)
+y_pred = best_model.predict(X_test)
+print('=== HASIL EVALUASI MODEL TERHADAP DATA UJI ===')
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Hasil rmse model lasso adalah {rmse:.4f}')
+print(f'Hasil mae model lasso adalah {mae:.4f}')
+print(f'Hasil r2 model lasso adalah {r2:.4f}')
+print(f'Harga Aktual pada data uji : \n {y_test.values}')
+print(f'Harga Prediksi pada data uji : \n {np.round(y_pred,2)}')
+
+# Visualisasi Nilai Prediksi vs Nilai Aktual
+seaborn_version = 'seaborn-v0_8-whitegrid'
+
+plt.figure(figsize=(8,8))
+plt.style.use(seaborn_version)
+
+sns.scatterplot(x=y_test, y=y_pred, color='dodgerblue', alpha=0.7)
+max_val = max(y_test.max(), y_pred.max())
+y_pred.max()
+
+plt.plot([0, max_val],[0, max_val], 'r--', lw=2, label='Garis prediksi sempurna (y=x)')
+plt.title('Prediksi vs Nilai Aktual', fontsize=16, fontweight='bold')
+plt.xlabel('Harga Aktual',fontsize=12)
+plt.ylabel('Harga Prediksi', fontsize=12)
+plt.legend()
+plt.text(0.05, 0.9, f'R2{r2:.4f}', transform=plt.gca(). transAxes, fontsize=12, verticalalignment='top')
+plt.show()
+

324c732d5b3169210b6eb407893c9488e8df4207
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-17 16:49:17 +0800

    Rename Repo

---
+++ Lasso Regression/src/Evaluate.py
@@ -0,0 +1,100 @@
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import matplotlib as plt
+import seaborn as plt
+
+print('Best params lasso :', grid_lasso.best_params_)
+print('Best CV Score :', grid_lasso.best_score_)
+print(best_model)
+y_pred = best_model.predict(X_test)
+print('=== HASIL EVALUASI MODEL TERHADAP DATA UJI ===')
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Hasil rmse model lasso adalah {rmse:.4f}')
+print(f'Hasil mae model lasso adalah {mae:.4f}')
+print(f'Hasil r2 model lasso adalah {r2:.4f}')
+print(f'Harga Aktual pada data uji : \n {y_test.values}')
+print(f'Harga Prediksi pada data uji : \n {np.round(y_pred,2)}')
+
+# Visualisasi Nilai Prediksi vs Nilai Aktual
+seaborn_version = 'seaborn-v0_8-whitegrid'
+
+plt.figure(figsize=(8,8))
+plt.style.use(seaborn_version)
+
+sns.scatterplot(x=y_test, y=y_pred, color='dodgerblue', alpha=0.7)
+max_val = max(y_test.max(), y_pred.max())
+y_pred.max()
+
+plt.plot([0, max_val],[0, max_val], 'r--', lw=2, label='Garis prediksi sempurna (y=x)')
+plt.title('Prediksi vs Nilai Aktual', fontsize=16, fontweight='bold')
+plt.xlabel('Harga Aktual',fontsize=12)
+plt.ylabel('Harga Prediksi', fontsize=12)
+plt.legend()
+plt.text(0.05, 0.9, f'R2{r2:.4f}', transform=plt.gca(). transAxes, fontsize=12, verticalalignment='top')
+plt.show()
+
+# Visualisasi Distribusi Residual Error
+
+residual = y_test - y_pred
+
+plt.figure(figsize=(8,6))
+plt.style.use(seaborn_version)
+
+sns.histplot(residual, kde=True, color='coral', bins=30)
+plt.title('Distribusi Residual Error', fontsize=16, fontweight='bold')
+plt.xlabel('Residual (Harga Aktual - Harga Prediksi)', fontsize=12)
+plt.axvline(0, color='r', linestyle='--', label='Pusat di Nole')
+plt.legend()
+plt.show()
+
+# Visualisasi Residual vs Nilai Prediksi
+plt.figure(figsize=(8,6))
+plt.style.use(seaborn_version)
+
+sns.scatterplot(x=y_pred, y=residual, color='forestgreen', alpha=0.6)
+plt.axhline(0, color='r', linestyle='--', lw=2)
+plt.title('Residual vs Nilai Prediksi', fontsize=16, fontweight='bold')
+plt.xlabel('Harga Prediksi', fontsize=12)
+plt.ylabel('Residual', fontsize=12)
+plt.show()
+
+# Mengambil langkah preprocessor dan regressornya
+
+preprocessor = best_model.named_steps['preprocessor']
+regressor = best_model.named_steps['regressor'].regressor_ # Ditambahkan karena regressor di dalam TransformedTargetResessor
+features_name =[]
+
+# Numeric
+num_features = preprocessor.transformers_[0][2]
+features_name.extend(num_features)
+
+# Ordinal
+ord_features = preprocessor.transformers_[1][2]
+features_name.extend(ord_features)
+
+# OneHotEncoder
+cat_encoder = preprocessor.transformers_[2][1]
+cat_features = cat_encoder.get_feature_names_out(preprocessor.transformers_[2][2])
+features_name.extend(cat_features)
+
+# Buat DataFrame Feature Importancesnya
+
+coef = regressor.coef_
+feature_importance = pd.DataFrame({
+    'Feature': features_name,
+    'Coefficient': coef
+})
+feature_importance['Abs_Coefficient'] = feature_importance['Coefficient'].abs()
+feature_importance = feature_importance.sort_values('Abs_Coefficient', ascending=False)
+feature_importance.head(10)
+
+# Visualisasi Feature Importances
+plt.figure(figsize=(10,6))
+plt.barh(feature_importance['Feature'], feature_importance['Coefficient'])
+plt.xlabel('Coefficient Value')
+plt.ylabel('Feature')
+plt.title('Feature Importance (Lasso Regression Coefficient)')
+plt.gca().invert_yaxis()
+plt.show()

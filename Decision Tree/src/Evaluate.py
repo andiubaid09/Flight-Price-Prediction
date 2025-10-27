f5d21dd6e15ced98c3160dc86b52194befd33c00
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-27 16:52:22 +0800

    Update Evaluate

--- Decision Tree/src/Evaluate.py
+++ Decision Tree/src/Evaluate.py
@@ -32,3 +32,63 @@ plt.ylabel('Harga Prediksi', fontsize=12)
 plt.legend()
 plt.text(0.05,0.9, f'R2:{r2:.4f}', transform=plt.gca(). transAxes, fontsize=12, verticalalignment='top')
 plt.show()
+
+residual = y_test - y_pred
+
+plt.figure(figsize=(8,6))
+plt.style.use(seaborn_version)
+
+sns.histplot(residual, kde=True, color='coral', bins=30)
+plt.title('Distribusi Residual Error', fontsize=16, fontweight='bold')
+plt.xlabel('Residual (Harga Aktual - Harga Prediksi)', fontsize=12)
+plt.axvline(0, color='r', linestyle='--', label='Pusat di Nol')
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+plt.style.use(seaborn_version)
+
+sns.scatterplot(x=y_pred, y=residual, color='forestgreen', alpha=0.6)
+plt.axhline(0, color='r', linestyle='--', lw=2)
+plt.title('Residual vs Nilai Prediksi', fontsize=16, fontweight='bold')
+plt.xlabel('Harga Prediksi', fontsize=12)
+plt.ylabel('Residual', fontsize=12)
+plt.show()
+
+# Mengambil step preprocessor dan regressor (Decision Tree)
+
+preprocessor = best_dt.named_steps['preprocessor']
+model = best_dt.named_steps['regressor'].regressor_
+
+# Ambil fitur numerik
+num_features = preprocessor.transformers_[0][2]
+
+# Ambil fitur ordinal
+ord_features = preprocessor.transformers_[1][2]
+
+# Ambil fitur kategori (one-hot)
+ohe = preprocessor.transformers_[2][1]
+cat_features = preprocessor.transformers_[2][2]
+ohe_features = list(ohe.get_feature_names_out(cat_features))
+
+
+# Gabungkan semuanya
+all_features = list(num_features) + list(ord_features) + ohe_features
+# Feature importance dari model decision tree
+importances = model.feature_importances_
+
+# Buat dataframe
+feature_importance_df = pd.DataFrame({
+    'Feature': all_features,
+    'Importance': importances
+}).sort_values(by='Importance', ascending=False).reset_index(drop=True)
+feature_importance_df.head(10)
+feature_importance_df
+
+plt.figure(figsize=(10,6))
+sns.barplot(data=feature_importance_df.head(15), x='Importance', y='Feature')
+plt.title('Top 15 Feature Importances - Decision Tree')
+plt.xlabel('Importance Score')
+plt.ylabel('Features Name')
+plt.tight_layout()
+plt.show()

530f47249d01f4a70076c379b7e6fb798824c3ea
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-29 10:29:48 +0800

    Update src Hyperparameter Tuning.py

--- XGBoost/src/Hyperparameter Tuning.py
+++ XGBoost/src/Hyperparameter Tuning.py
@@ -1,1 +1,16 @@
 from sklearn.model_selection import GridSearchCV
+
+params_grid_XGB = {
+    'regressor__regressor__n_estimators' : [150, 200, 300],
+    'regressor__regressor__learning_rate': [0.05, 0.1, 0.2],
+    'regressor__regressor__max_depth' : [10, 15, 20],
+}
+
+gridsearch_XGB = GridSearchCV(
+    estimator= XGB_pipeline,
+    param_grid = params_grid_XGB,
+    cv = 5,
+    scoring = 'r2',
+    n_jobs=-1,
+    verbose=2
+)

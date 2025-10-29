38b6c2273afe67be5cd07f43449c09e718dfaec5
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-29 10:00:16 +0800

    Update src Preprocessing.py

--- XGBoost/src/Preprocessing.py
+++ XGBoost/src/Preprocessing.py
@@ -7,3 +7,22 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder,
 from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 from xgboost import XGBRegressor
+
+path = kagglehub.dataset_download('rohitgrewal/airlines-flights-data')
+print('Path dataset berada di sini', path)
+print('Isi dari path ini adalah', os.listdir(path))
+df = pd.read_csv(path + '/airlines_flights_data.csv')
+df
+df.drop(columns='index', inplace=True)
+df.head(5)
+df.columns
+
+Features = ['source_city','departure_time','stops','arrival_time',
+            'destination_city','class','days_left']
+X = df[Features]
+y = df['price']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+print(f"Data training berjumlah :{len(X_train)} dan data uji berjumlah : {len(X_test)}")

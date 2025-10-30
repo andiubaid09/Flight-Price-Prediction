5572f7566ed261176e9cce4fe94cb066de25b044
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-30 10:46:46 +0800

    Add src Preprocessing.py

---
+++ LightGBM/src/Preprocessing.py
@@ -0,0 +1,9 @@
+import kagglehub
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder, FunctionTransformer
+from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
+from sklearn.pipeline import Pipeline
+from lightgbm import LGBMRegressor

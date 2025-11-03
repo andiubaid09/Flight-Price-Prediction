ba9b3f2ad5d3d35c896b13900be4ebdb2b2e64d4
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-11-03 21:42:33 +0800

    Add src Preprocessing.py

--- Catboost Regression/src/Preprocessing.py
+++ Catboost Regression/src/Preprocessing.py
@@ -18,3 +18,51 @@ df.head(5)
 Features = ['source_city','departure_time','stops','arrival_time','destination_city','class','days_left']
 X = df[Features]
 y = df['price']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2,  random_state=42
+)
+print(f"Jumlah data training {len(X_train)} dan jumlah data uji {len(X_test)}")
+
+scaler = StandardScaler()
+num_feat = ['days_left']
+num_transform = scaler
+
+encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+cat_feat = ['source_city', 'departure_time','stops','arrival_time','destination_city']
+cat_transform = encoder
+
+ordinal_feat = ['class']
+class_categ = ['Economy','Business']
+ordinal_transform = OrdinalEncoder(categories=[class_categ])
+
+log_transform = FunctionTransformer(np.log1p, inverse_func=np.expm1)
+
+preprocessor = ColumnTransformer([
+    ('num', Pipeline([
+        ('log',log_transform),
+        ('scaler', num_transform)
+    ]), num_feat),
+    ('ord', ordinal_transform, ordinal_feat),
+    ('cat', cat_transform, cat_feat)
+], remainder = 'drop')
+
+!pip install catboost
+
+cbm = CatBoostRegressor(
+    loss_function = 'RMSE',
+    eval_metric = 'R2',
+    random_seed = 42,
+    early_stopping_rounds=50
+)
+
+model_cat = TransformedTargetRegressor(
+    regressor = cbm,
+    func = np.log1p,
+    inverse_func= np.expm1
+)
+
+cbm_pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', model_cat)
+])

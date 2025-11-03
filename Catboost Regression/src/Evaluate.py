72a6d7aee06b9035696714ac7ff804c6e3a3254e
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-11-03 21:48:58 +0800

    Add src Evaluate

---
+++ Catboost Regression/src/Evaluate.py
@@ -0,0 +1,7 @@
+import matplotlib as plt
+import seaborn as sns
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+print('Best Params yang ditemukan', random_cbm.best_params_)
+print('CV Score yang ditemukan', random_cbm.best_score_)
+print(best_cbm)

222c6a6fbe50afc39f1f45bf403f5421e0f3bd25
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-11-03 21:40:28 +0800

    Add src Preprocessing.py

--- Catboost Regression/src/Preprocessing.py
+++ Catboost Regression/src/Preprocessing.py
@@ -7,3 +7,14 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder,
 from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 from catboost import CatBoostRegressor
+
+path = kagglehub.dataset_download('rohitgrewal/airlines-flights-data')
+print("Lokasi File Berada di Path berikut :", path)
+print("Isi Path tersebut adalah", os.listdir(path))
+df = pd.read_csv(path + '/airlines_flights_data.csv')
+df
+df.drop(columns = 'index', inplace=True)
+df.head(5)
+Features = ['source_city','departure_time','stops','arrival_time','destination_city','class','days_left']
+X = df[Features]
+y = df['price']
